d53678a345bd70a3253571fd005237397bd76123
Stepan Hurt <stepanhurt@seznam.cz>
2020-05-21 20:11:11 +0200

    new version sčítání a odčítání

--- src/cz/hurt/calculator/CalculatorApp.kt
+++ src/cz/hurt/calculator/CalculatorApp.kt
@@ -1,2 +1,26 @@
 package cz.hurt.calculator
+sealed class Operator(val x: Long) {
+    abstract fun calculate(y: Long): Long
+
+    class add(x: Long) : Operator(x) {
+        override fun calculate(y: Long): Long {
+            return x + y
+        }
+    }
+    class sub(x: Long) : Operator(x) {
+        override fun calculate(y: Long): Long {
+            return x - y
+        }
+    }
+    class mult(x: Long) : Operator(x) {
+        override fun calculate(y: Long): Long {
+            return x * y
+        }
+    }
+    class div(x: Long) : Operator(x) {
+        override fun calculate(y: Long): Long {
+            return x / y
+        }
+    }
+}
 
