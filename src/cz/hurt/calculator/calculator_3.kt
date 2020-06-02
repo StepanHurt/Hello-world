5a14ac461eabd99799e556f33a56eb2aaa8f29b4
Stepan Hurt <stepanhurt@seznam.cz>
2020-06-02 22:15:08 +0200

    verze 3

---
+++ src/cz/hurt/calculator/calculator_3.kt
@@ -0,0 +1,44 @@
+package cz.hurt.calculator
+
+import java.util.Scanner
+
+fun main(args:Array<String>){
+
+    val reader = Scanner(System.`in`)
+    println("-----Welcome to the Calcuator App-----")
+    println("Enter two numbers: ")
+
+
+
+    while(true)
+    {
+
+        print("First Number: ")
+        val x = reader.nextDouble()
+        print("Second Number: ")
+        val y = reader.nextDouble()
+
+        println("\n1. Addition")
+        println("2. Subtraction")
+        println("3. Multiplication")
+        println("4. Division")
+        println("5. Quit")
+
+        print("Enter your choice: ")
+        val ch = readLine()
+
+        println()
+        when(ch){
+
+            "1" -> println(x+y)
+            "2" -> println(x-y)
+            "3" -> println(x*y)
+            "4" -> println(x/y)
+            "5" -> return
+
+        }
+    }
+
+
+
+}
